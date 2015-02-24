ed285c29227a5389007fba50de0734255c6f226e
Jisse Reitsma <info@yireo.com>
2015-02-24 13:12:49 +0100

    Add book banner

--- index.php
+++ index.php
@@ -47,9 +47,20 @@ $slideGroups = array(
   <body>
     <div class="container container-heading">
     <img src="images/yireo-logo.png" align="right" />
-    <h1>Yireo Slides <small>based on remark</small></h1>
+    <h1>Yireo Slides <small>built with <a target="_new" href="http://remarkjs.com/#1">remark</a></small></h1>
     </div>
     <div class="container">
+    <div class="box-gradient advertizement">
+        <div class="content">
+            <img src="images/book.png" />
+            <h2>THE Book for Joomla Devs</h2>
+            <p>
+                360+ pages of guided programming<br/>
+                complete reference for plugins &amp; events<br/>
+                <a href="https://www.yireo.com/jpb">Get your copy now</a>
+            </p>
+        </div>
+    </div>
     <?php foreach($slideGroups as $slideGroup): ?>
     <h3><?php echo $slideGroup['group']; ?></h3>
     <ul>
