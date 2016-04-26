6b65eaf2df56518139c2db1778f7335dc6398835
Jisse Reitsma <jisse@yireo.com>
2016-04-26 06:48:04 +0200

    pending changes

--- definitions.php
+++ definitions.php
@@ -4,7 +4,7 @@ $slideGroups = array(
         'group' => 'Yireo on Joomla and Magento',
         'style' => 'yireo',
         'slides' => array(
-            array('file' => 'yireo/running_m2', 'title' => 'Running Magento 2 (Magento Developers Paradise 2016)', 'public' => 0),
+            array('file' => 'yireo/running_m2', 'title' => 'Running Magento 2 (Magento Developers Paradise 2016)', 'public' => 1),
             array('file' => 'yireo/messing_up_di', 'title' => 'Messing Up DI (MeetMagento NL 2016)', 'public' => 0),
             array('file' => 'yireo/php7', 'title' => 'PHP 7 (JoomlaDagen 2016)', 'public' => 1),
             array('file' => 'yireo/plugin_patterns', 'title' => 'Joomla Plugin Concepten (JUG Utrecht)'),
