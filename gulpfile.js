e452b15cc001e2a2b928a622049d821ba137a1b5
Jisse Reitsma <jisse@yireo.com>
2019-09-05 07:10:06 +0200

    Pending changes

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var watchLess = require('gulp-watch-less');
+var less = require('gulp-less');
+
+gulp.task('less', function () {
+    return gulp.src('less/style.less')
+        .pipe(watchLess('less/style.less'))
+        .pipe(less())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./*.less', ['less']);
+});
+
+gulp.task('default', ['watch']);
