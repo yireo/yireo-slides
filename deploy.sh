bec821e445037841ab58ac482657b9d225fdf438
jisse Reitsma <jisse@yireo.com>
2022-06-30 11:10:02 +0200

    Pending changes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ssh yireo-php 'cd /home/yireo/public_html/yireo-slides && git pull origin master && composer install'
 
-yireo opcache:refresh 8.1
-#ssh root@yireo "/etc/init.d/php73-php-fpm reload"
+yireo opcache:refresh 7.4
